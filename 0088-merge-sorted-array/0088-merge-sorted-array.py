f25c56c1ecccdfcfd17d7547215a8d87169a5252
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-03-25 10:13:48 +0530

    Time: 0 ms (100%), Space: 12.4 MB (44.1%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        arr=[]
+        for val in nums1[:m]: 
+            arr.append(val)
+        for val2 in nums2:
+            arr.append(val2)
+        arr.sort()
+        for i in range(len(arr)):
+            nums1[i]= arr[i]
+
+
