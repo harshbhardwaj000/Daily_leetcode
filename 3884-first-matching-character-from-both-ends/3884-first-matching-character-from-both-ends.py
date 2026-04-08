c881bc63992c3b2ee12787542b27b6352fd941f8
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-04-08 22:11:40 +0530

    Time: 2 ms (21.96%), Space: 12.3 MB (56.16%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def firstMatchingIndex(self, s):
+        n= len(s)
+        if len(s) == 1:
+                return 0
+        for i in range(len(s)-1):
+            if s[i] == s[n - i - 1]:
+                return i
+        return -1
+        
