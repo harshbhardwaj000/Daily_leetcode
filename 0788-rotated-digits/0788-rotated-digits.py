9e69660c89f00859344d6fedf182c74622616218
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-05-02 21:44:11 +0530

    Time: 31 ms (80.88%), Space: 12.6 MB (89.71%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotatedDigits(self, n):
+        count=0
+        for num in range(1,n+1):
+            s=str(num)
+            valid=True
+            changed=False
+            for ch in s:
+                if ch in '347':
+                    valid = False
+                    break
+                if ch in '2569':
+                    changed=True
+            if valid and changed:
+                count +=1
+        return count
+
+             
+        
