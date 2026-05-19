381ef2c0dc898830ba0e6c05cfc82f705987c3fe
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-05-19 21:29:00 +0530

    Time: 7 ms (97.46%), Space: 24.2 MB (58.47%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -2,7 +2,9 @@ class Solution(object):
     def getCommon(self, nums1, nums2):
         left =0
         right=0
-        while left <len(nums1) and right <len(nums2):
+        n1=len(nums1)
+        n2=len(nums2)
+        while left < n1 and right < n2:
             if nums1[left] == nums2[right]:
                 return nums1[left]
             if nums1[left] > nums2[right]:
