0450d2309fb44770ee761b69eaa418ce121d9cf9
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-05-20 14:34:40 +0530

    Time: 7 ms (53.5%), Space: 12.9 MB (17.83%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -2,14 +2,17 @@ from collections import Counter
 class Solution(object):
     def countWords(self, words1, words2):
         count1=Counter(words1)
-        ans=[x for x in words1 if count1[x] <= 1] #["leetcode","amazing","as"]
+        # ans=[x for x in words1 if count1[x] <= 1] #["leetcode","amazing","as"]
         
         count2=Counter(words2)
-        ans1=[x for x in words2 if count2[x] <=1] #["amazing","leetcode","is"]
+        # ans1=[x for x in words2 if count2[x] <=1] #["amazing","leetcode","is"]
         
         count=0
-        for X in ans:
-            if X in ans1:
+        # for X in ans:
+        #     if X in ans1:
+        #         count+=1
+        for word in count1:
+            if count1[word] == 1 and count2[word] == 1:
                 count+=1
         return count
 
