4065346184b47b5025608d93942aa1e0c20e879e
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-05-27 09:57:31 +0530

    Time: 0 ms (100%), Space: 12.4 MB (59.12%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -2,11 +2,11 @@ class Solution(object):
     def capitalizeTitle(self, title):
         s= title.lower().split() ####["hello","world"]
         for i in range(len(s)):
-            s[i]=s[i][0].upper() + s[i][1:]
             if len(s[i]) <= 2:
                 s[i]=s[i].lower()
-        result = " ".join(s)
-        return result
+            else:
+                s[i]=s[i][0].upper() + s[i][1:]
+        return " ".join(s)
                  
 
         
