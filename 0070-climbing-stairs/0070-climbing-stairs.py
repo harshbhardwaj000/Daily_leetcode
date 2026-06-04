5b2b4849ce4735869b514678535726929dfc2562
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-06-04 12:32:13 +0530

    Time: 0 ms (100%), Space: 12.5 MB (19.86%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if (n == 0) or (n == 1):
+            return 1
+        pre =1
+        pre_pre=1
+        for i in range(2,n+1):
+
+            curr = pre + pre_pre  ## 2
+            pre_pre = pre
+            pre=curr
+        return curr
+
+        
