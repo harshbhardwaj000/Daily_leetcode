dc0f4539e523b92c99307e26bd11d71b9c068537
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-06-16 18:57:24 +0530

    Time: 56 ms (61.54%), Space: 22.2 MB (76.92%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -9,10 +9,9 @@ class Solution(object):
                 if result:
                     result.pop()
             elif ch == "#":
-                result = result + result
-            elif ch == "%":
-                result= result[::-1]
-        # if not result:
-        #         return s
+                result.extend(result)
+            else:
+                result.reverse()
+
         return "".join(result)
         
