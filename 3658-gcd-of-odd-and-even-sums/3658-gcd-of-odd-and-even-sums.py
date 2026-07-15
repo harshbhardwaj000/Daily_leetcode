f06a511c4f4246c4e3db99edc7414a447c68dbcf
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-07-15 22:13:34 +0530

    Time: 37 ms (19.8%), Space: 12.5 MB (5.7%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def gcdOfOddEvenSums(self, n):
+        count_odd =0
         count_even=0
         for i in range(n*2):
-            if i%2 != 0:
+            if i%2 == 0:
                 count_even += i
             else:
-                count_even -= i
-        return count_even
+                count_odd += i
+        return count_odd - count_even
         
         
