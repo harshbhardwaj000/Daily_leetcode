2c4e2d3bf3d407db420812620d40c546fbb54d4e
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-07-16 11:24:32 +0530

    Time: 0 ms (100%), Space: 12.5 MB (71.4%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def runningSum(self, nums):
-        prefix = [0]*(len(nums)+1)
-        for i in range(len(nums)):
-            prefix[i+1] = prefix[i] + nums[i]
-        return prefix[1:]
-        
-
-
+        n=len(nums)
+        res = [0]*n
+        res[0]=nums[0]
+        for i in range(1,n):
+            res[i]= res[i-1] + nums[i]
+        return res
         
