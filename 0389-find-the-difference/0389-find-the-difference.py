3c9e0bfa3d5365d8143f91f3635e88d982dc292c
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-07-20 23:43:23 +0530

    Time: 6 ms (29.92%), Space: 12.3 MB (65.83%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,11 +1,12 @@
+from collections import Counter
 class Solution(object):
     def findTheDifference(self, s, t):
-        m=list(s)
-        n=list(t)
-        for val in n:
-            if val in m:
-                m.remove(val)
-            else:
-                return val
-
-        
+        count_s = Counter(s)
+        count_t = Counter(t)
+        for key in count_t:
+            if key in count_s:
+                if count_t[key] != count_s[key]:
+                    return key
+            elif key not in count_s:
+                return key
+            
