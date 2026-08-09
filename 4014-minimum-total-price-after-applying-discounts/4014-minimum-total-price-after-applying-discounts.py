bbedc2101810e8d49ce96543d3951d21bb2d9d19
Harsh Bhardwaj <harsh639bhardwaj@gmail.com>
2026-08-10 01:36:40 +0530

    Time: 285 ms (100%), Space: 22 MB (100%) - LeetHub

---
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minPrice(self, prices, discounts):
+        final_sum = 0
+        n = len(prices)
+        m = len(discounts)
+        k= n-m
+        prices.sort(reverse=True)
+        if k > 0:
+            dis = sorted(discounts,reverse=True) + [0]*k
+        else:
+            dis = sorted(discounts,reverse=True)
+        
+        for i in range(len(prices)):
+            avg = (prices[i]*(100-dis[i])) / 100.0
+            final_sum += avg
+        return final_sum
+
+
+        
